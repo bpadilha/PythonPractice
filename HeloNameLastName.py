b04d0c6f464e524b29ffc1465022d527af9ba838
bpadilha <padilha.bpr@gmail.com>
2019-04-11 22:39:07 -0300

    First exercise, say hello to a name and lastname

---
+++ HeloNameLastName.py
@@ -0,0 +1,6 @@
+##Write a program that read the name and last name and say hello to this name
+
+name = str(input("Say your name: "))
+lastname = str(input("Say your last name: "))
+
+print(f"Hello, {name} {lastname}.")
