c310831d1c978797fb6584f43227fe35a29e8c4f
bpadilha <padilha.bpr@gmail.com>
2019-04-12 12:02:36 -0300

    A program that returns a new favorite number

---
+++ newFavoriteNumer.py
@@ -0,0 +1,13 @@
+#write a question that asks for a number and sums 1 and return a new favorite number
+
+def favorite(n):
+    n = n +1
+    print (f"Your new favorite number is {n}")
+
+number = int(input('What is your favorite number: '))
+
+# i = -5
+# while i < 15:
+#
+#     favorite(i)
+#     i += 1
